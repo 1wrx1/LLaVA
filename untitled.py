0afca32185df5a4c1d331c9c4bf0b9669d3fff3b
1wrx1 <111wrx111@gmail.com>
2025-02-18 10:32:39 +0800

    Initial commit

---
+++ untitled.py
@@ -0,0 +1,15 @@
+from llava.model.builder import load_pretrained_model
+from llava.mm_utils import get_model_name_from_path
+from llava.eval.run_llava import eval_model
+import os
+
+model_path = "liuhaotian/llava-v1.5-7b"
+
+os.environ['TRANSFORMERS_CACHE'] = '/grp01/saas_lqqu/runxi/llava/.cache'
+
+tokenizer, model, image_processor, context_len = load_pretrained_model(
+    model_path=model_path,
+    model_base=None,
+    model_name=get_model_name_from_path(model_path),
+    cache_dir='/grp01/saas_lqqu/runxi/llava/.cache'
+)
