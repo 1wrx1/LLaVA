f6f4b34c715d6eb5fbc39c3d667109aace2d48e7
1wrx1 <111wrx111@gmail.com>
2025-02-18 19:09:23 +0800

    commit

--- untitled.py
+++ untitled.py
@@ -3,7 +3,7 @@ from llava.mm_utils import get_model_name_from_path
 from llava.eval.run_llava import eval_model
 import os
 
-model_path = "liuhaotian/llava-v1.5-7b"
+model_path = "liuhaotian/llava-v1.5-13b"
 
 os.environ['TRANSFORMERS_CACHE'] = '/grp01/saas_lqqu/runxi/llava/.cache'
 
